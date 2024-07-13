94e13945bc0dbe073c3898b82de6939101055897
we-make-software <hello@how-do-i-get-your-attention.com>
2024-07-13 17:23:57 -0600

    Update

--- WMS Hardware Binding/.c
+++ WMS Hardware Binding/.c
@@ -53,9 +53,11 @@ VOID EvtDriverUnload(_In_ WDFDRIVER Driver) {
     UNREFERENCED_PARAMETER(Driver);
     DbgPrint("WDF DriverUnload\n");
 }
+DRIVER_INITIALIZE DriverEntry;
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath) {
     WDF_DRIVER_CONFIG config;
     WDF_DRIVER_CONFIG_INIT(&config, EvtDriverDeviceAdd);
     config.EvtDriverUnload = EvtDriverUnload;
-    return WdfDriverCreate(DriverObject, RegistryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, WDF_NO_HANDLE);
+    WdfDriverCreate(DriverObject, RegistryPath, WDF_NO_OBJECT_ATTRIBUTES, &config, WDF_NO_HANDLE);
+    return STATUS_SUCCESS;
 }
